c3ea2c822dc6add63636f5d628b2c2c0072050e9
aashiqmustak <aashiqmustak5969@gmail.com>
2026-05-27 10:06:51 +0530

    money splitting

---
+++ money.py
@@ -0,0 +1,15 @@
+money = 100
+
+n500 = money // 500
+money = money % 500
+
+
+n100 = money // 100
+money = money % 100
+
+n200 = money // 200
+money = money % 200
+
+print("500 rs:", n500)
+print("100 rs:", n100)  
+print("200 rs:", n200)
