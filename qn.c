4fa11e32e2d4813c340e6e379fefb3bb2ea4367e
aashiqmustak <aashiqmustak5969@gmail.com>
2026-05-29 09:44:13 +0530

    maximum in an array

---
+++ qn.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a[5]= [10,3,70,990,100];
+    int max = a[0];
+
+    for (int i = 0 ; i < a ; i++)
+            if(a[i]>max{
+                max = a[i];
+                
+            })
+        }
---
+++ trick.py
@@ -0,0 +1,26 @@
+# a = 5 
+
+# for i in range(1,a+1):
+#     if(i %2 ==0):
+#         print(a+i)
+#     else:
+#         print(a
+              
+#               *i)
+
+
+
+n = 1
+s = 0
+
+while n > 0 :
+    n = int(input("Enter a number: "))
+    s = s + n
+print(s+1)
+
+
+print("the sum is ",s+1 )
+
+
+
+
