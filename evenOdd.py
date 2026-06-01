ad7524abbbb69f82ce54134afc445d2c64969ec5
aashiqmustak <aashiqmustak5969@gmail.com>
2026-06-01 13:56:59 +0530

    trick(even-odd) using array

--- evenOdd.py
+++ evenOdd.py
@@ -1,7 +1,13 @@
-a = 10 ;
+# a = 10 ;
 
-if(a%2 == 0):
-    print(a," is even"
-          )
-else:
-    print(a," is odd")
+# if(a%2 == 0):
+#     print(a," is even"
+#           )
+# else:
+#     print(a," is odd")
+
+
+n = int(input("Enter a number: "))
+
+a = ['Even','Odd']
+print(n," is ",a[n%2])
