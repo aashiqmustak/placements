316e3b811728f73586cff39fd86ca842f1d8ccf3
aashiqmustak <aashiqmustak5969@gmail.com>
2026-06-03 16:04:34 +0530

    push

---
+++ binarytrick.py
@@ -0,0 +1,15 @@
+# a = int(input("Enter a number: "))
+# b = [64,32,16,8,4,2,1]
+
+# count = 0 
+
+# for i in range(0,len(b)):
+#     if(a>=b[i]):
+#         count = count + 1
+#         a = a - b[i]
+# print(count)
+
+a = int(input("Enter a number: "))
+b = bin(a)
+
+print(b)
