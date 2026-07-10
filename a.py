195f854ba1fe0071292e6b0585c50fc4ce780aab
aashiqmustak <aashiqmustak5969@gmail.com>
2026-07-10 12:35:23 +0530

    div3

--- a.py
+++ a.py
@@ -1,4 +1,16 @@
-a  = 8 
+n = int(input())
+a = list(map(int, input().split()))
+count1=0
+count2=0
+count3=0
+for i in range(len(a)):
+    if(a[i]%3==0):
+        count1 +=1
+    elif(a[i]%3==1):
+        count2+=1
+    else:
+        count3+=1
 
-b = a +a
-print(a)
+    
+ans = count1+min(count2,count3)
+print(ans)
