1b6de3e1e9f59b158ffbdc5fa0e85e6904d8e26e
aashiqmustak <aashiqmustak5969@gmail.com>
2026-08-26 23:33:35 +0530

    l:

--- average.py
+++ average.py
@@ -1,9 +1,24 @@
-a = 10
-b= 20
-c= 30
-d= 40
-e= 50
+# a = 10
+# b= 20
+# c= 30
+# d= 40
+# e= 50
 
-avg = (a + b + c + d + e) / 5
-print("Average = ",avg)
-print()
+# avg = (a + b + c + d + e) / 5
+# print("Average = ",avg)
+# print()
+
+n = int(input())
+
+l = list(map(int, input().split()))   # leaving
+w = list(map(int, input().split()))   # entering
+
+rem = 0
+maxi = 0
+
+for i in range(n):
+    rem = rem + l[i] - w[i]
+    if rem > maxi:
+        maxi = rem
+
+print(maxi)
